a207e3966a637ee2471d41936a0012f6e969bf92
LuizOMartinsProver <loliveira@provertec.com.br>
2022-12-22 11:54:36 -0300

    Criação do componente de Listagem

--- src/app/Repository/UsuarioService.ts
+++ src/app/Repository/UsuarioService.ts
@@ -11,29 +11,29 @@ export class UsuarioService{
     }
 
     getUsuarios(){
-        return this.http.get('http:localhost:3000/usuarios')
+        return this.http.get<UsuarioModel[]>('http://localhost:3000/usuarios');
     }
 
     getUsuario(id:number){
-        return this.http.get(`http:localhost:3000/usuarios/${id}`);
+        return this.http.get<UsuarioModel>(`http://localhost:3000/usuarios/${id}`);
     }
 
     addUsuario(record: UsuarioModel){
         let headers =  new HttpHeaders();
         headers =  headers.set('Content-Type', 'application/json; charset=utf-8');
-        return this.http.post('http:localhost:3000/usuarios', JSON.stringify(record), {headers: headers});
+        return this.http.post('http://localhost:3000/usuarios', JSON.stringify(record), {headers: headers});
     }
 
     updateUsuario(record: UsuarioModel){
         let headers =  new HttpHeaders();
         headers =  headers.set('Content-Type', 'application/json; charset=utf-8');
-        return this.http.put('http:localhost:3000/usuarios/'+record.id, JSON.stringify(record), {headers: headers});
+        return this.http.put('http://localhost:3000/usuarios/'+record.id, JSON.stringify(record), {headers: headers});
     }
 
     deleteUsuario(id: number){
         let headers =  new HttpHeaders();
         headers =  headers.set('Content-Type', 'application/json; charset=utf-8');
-        return this.http.delete('http:localhost:3000/usuarios/'+id, {headers: headers});
+        return this.http.delete('http://localhost:3000/usuarios/'+id, {headers: headers});
     }
 
 }
--- src/app/listar-usuarios/listar-usuarios.component.ts
+++ src/app/listar-usuarios/listar-usuarios.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { UsuarioModel } from './../Models/UsuarioModel';
+
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../Repository/UsuarioService';
 
 @Component({
   selector: 'app-listar-usuarios',
   templateUrl: './listar-usuarios.component.html',
   styleUrls: ['./listar-usuarios.component.css']
 })
-export class ListarUsuariosComponent {
+export class ListarUsuariosComponent implements OnInit {
+  listaUsuarios: UsuarioModel[] = [];
+
+
+  constructor(private usuarioService: UsuarioService){
+  }
+
+  ngOnInit(): void {
+      this.usuarioService.getUsuarios().subscribe((usuarios: UsuarioModel[]) =>{
+        this.listaUsuarios = usuarios;
+      });
+  }
 
 }
