625540374bad686bbcaaa327c9699b463e6c17ad
LuizOMartinsProver <loliveira@provertec.com.br>
2023-02-24 10:39:20 -0300

    Criando os Reducers

--- src/app/Store/usuarios/usuario.actions.ts
+++ src/app/Store/usuarios/usuario.actions.ts
@@ -25,67 +25,68 @@ export const enum usuariosTypeAction {
 
 // LOAD Usuarios
 export const LoadUsuarios = createAction(usuariosTypeAction.LOAD_USUARIOS);
+
 export const LoadUsuariosSuccess = createAction(
   usuariosTypeAction.LOAD_USUARIOS_SUCCESS,
-  props<{ payload: UsuarioModel[] }>
+  props<{ payload: UsuarioModel[] }>()
 );
 export const LoadUsuariosFail = createAction(
   usuariosTypeAction.LOAD_USUARIOS_FAIL,
-  props<{ error: String }>
+  props<{ error: string }>()
 );
 
 // LOAD USUARIO
 export const LoadUsuario = createAction(
   usuariosTypeAction.LOAD_USUARIO,
-  props<{ payload: number }>
+  props<{ payload: number }>()
 );
 export const LoadUsuarioSuccess = createAction(
   usuariosTypeAction.LOAD_USUARIO_SUCCESS,
-  props<{ payload: UsuarioModel }>
+  props<{ payload: UsuarioModel }>()
 );
 export const LoadUsuarioFail = createAction(
   usuariosTypeAction.LOAD_USUARIO_FAIL,
-  props<{ error: String }>
+  props<{ error: string }>()
 );
 
 // Create Usuario
 export const CreateUsuario = createAction(
   usuariosTypeAction.CREATE_USUARIO,
-  props<{ payload: UsuarioModel }>
+  props<{ payload: UsuarioModel }>()
 );
 export const CreateUsuarioSuccess = createAction(
   usuariosTypeAction.CREATE_USUARIO_SUCCESS,
-  props<{ payload: UsuarioModel }>
+  props<{ payload: UsuarioModel }>()
 );
 export const CreateUsuarioFail = createAction(
   usuariosTypeAction.CREATE_USUARIO_FAIL,
-  props<{ error: String }>
+  props<{ error: string }>()
 );
 
 // UPDATE Usuario
 export const UpdateUsuario = createAction(
   usuariosTypeAction.UPDATE_USUARIO,
-  props<{ payload: UsuarioModel }>
+  props<{ payload: UsuarioModel }>()
 );
 export const UpdateUsuarioSuccess = createAction(
   usuariosTypeAction.UPDATE_USUARIO_SUCCESS,
-  props<{ payload: UsuarioModel }>
+  props<{ payload: UsuarioModel }>()
 );
 export const UpdateUsuarioFail = createAction(
   usuariosTypeAction.UPDATE_USUARIO_FAIL,
-  props<{ error: String }>
+  props<{ error: string }>()
 );
 
 // DELETE Usuario
 export const DeleteUsuario = createAction(
   usuariosTypeAction.DELETE_USUARIO,
-  props<{ payload: number }>
+  props<{ payload: number }>()
 );
 export const DeleteUsuarioSuccess = createAction(
   usuariosTypeAction.DELETE_USUARIO_SUCCESS,
-  props<{ payload: number }>
+  props<{ payload: number }>()
 );
 export const DeleteUsuarioFail = createAction(
   usuariosTypeAction.DELETE_USUARIO_FAIL,
-  props<{ error: String }>
+  props<{ error: string }>()
 );
