b4552878becc7756763e2819fa05522ad06d263c
LuizOMartinsProver <loliveira@provertec.com.br>
2023-05-03 19:57:41 -0300

    Implementando Selector

--- src/app/listar-usuarios/listar-usuarios.component.ts
+++ src/app/listar-usuarios/listar-usuarios.component.ts
@@ -3,13 +3,19 @@ import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 import { AppState } from '../Store/app-state';
 import * as fromUsuariosAction from '../Store/usuarios/usuario.actions';
+import * as fromUsuariosSelector from '../Store/usuarios/usuarios.reducer';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-listar-usuarios',
   templateUrl: './listar-usuarios.component.html',
   styleUrls: ['./listar-usuarios.component.css'],
 })
 export class ListarUsuariosComponent implements OnInit {
-  listaUsuarios: UsuarioModel[] = [];
+  listaUsuarios$: Observable<UsuarioModel[]> = this.store.select(
+    fromUsuariosSelector.getUsuarios
+  );
+
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
